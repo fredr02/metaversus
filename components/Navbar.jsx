1c1a7ee5f9180aa78fa2604ee70960c5b40df7a6
Fred Lopez <frederickr02@gmail.com>
2023-01-30 10:25:22 -0500

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="aboslute w-[50%] inset-0"></div>
+  </motion.nav>
 );
 
 export default Navbar;
